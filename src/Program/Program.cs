dfa708623907d7b4c46475461ff905f858079df1
ManuelM512 <manuel5mongelos@gmail.com>
2022-08-31 10:38:21 -0300

    modified:   Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,6 @@ namespace PII_Game_Of_Life
     {
         static void Main(string[] args)
         {
-            //Que el gameboard pueda ser creado por diferentes maneras
             Reader lector = new Reader();
             GameBoard tablero = new GameBoard(lector.board);
             Printer impr = new Printer(tablero);
